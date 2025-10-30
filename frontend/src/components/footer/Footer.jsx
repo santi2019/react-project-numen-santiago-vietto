5a8c83050f92784f7b94c99a8ef83949cebdd763
santi2019 <santiagovietto5@hotmail.com>
2025-10-29 21:52:07 -0300

    Termino mainContent y cierro branch

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="mainMemberContainer">
+            <h3>BECOME A MEMBER AND EARN 250 POINTS TO SPEND ON REWARDS</h3>
+            <button>SIGN UP FOR FREE</button>
+        </div>
+        
+      </footer>
+    </>
+  )
+}
+
+export default Footer
